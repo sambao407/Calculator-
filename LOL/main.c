980c63e6254ab96b3eb048edd5856461d719dc28
Orlio261 <olophsonorlando.samba@viacesi.fr>
2016-11-04 17:16:25 +0100

    S et A

--- LOL/main.c
+++ LOL/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition(int a, int b)
+{
+return a+b;
+}
+int soustractioin(int c, int d)
+{
+    return c-d;
+}
 int main()
 {
     printf("Hello world!\n");
